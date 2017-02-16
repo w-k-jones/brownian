bb026298109cdb3cb8ef1e85ce5603e3cc3eef24
lucmoseley <luc.moseley100@gmail.com>
2017-02-16 15:27:47 +0000

    Updated so works with new animation file

--- brownian_system.py
+++ brownian_system.py
@@ -2,7 +2,9 @@
 """
 Created on Wed Jan 25 09:50:23 2017
 
-@author: pc
+@author: wj
+History:
+    16/02/2017, lm; Small adjustments so can run properly with animation file
 """
 #
 import numpy as np
@@ -29,19 +31,21 @@ class system:
             self.ball.p += self.ball.v*self.min_t
             if t_2col == self.min_t:
                 self.ball.dv_col()
-                print 'b'
+                #print 'b'
                 
             if t_2wall == self.min_t:
                 self.wall.dv_wall(self.ball)
-                print 'w'
+                #print 'w'
                 
             if t_2corn == self.min_t:
                 self.wall.dv_corn(self.ball)
-                print 'c'
+                #print 'c'
             
         else:
             self.t +=self.t_step
             self.ball.p += self.ball.v*self.t_step
+            
+        return self.ball.p, self.t
             
     def plt(self):
         fig = plt.figure(figsize=(6,6))
